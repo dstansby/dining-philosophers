9c8cb4ef44dd6fa8fd7c10c739f246de46befd25
Tim Spain <t.spain@ucl.ac.uk>
2020-03-02 12:04:16 +0000

    Add the mutex bound philosophers

---
+++ src_mutex/philosopher.cpp
@@ -0,0 +1,102 @@
+/*
+ * philosopher.cpp:
+ *
+ * Implementation of a dining philosopher
+ *
+ *  Created on: 31 Jan 2020
+ *      Author: tim
+ */
+
+#include "philosopher.hpp"
+
+#include <thread>
+#include <sstream>
+#include <iostream>
+
+Philosopher::Philosopher(std::string name, Chopstick *left, Chopstick *right)
+: name (name), left (left), right (right), have_l (false), have_r (false),
+  eat_time (std::chrono::milliseconds(20)),
+  think_time (std::chrono::milliseconds(30)),
+  hunger (0)
+{ }
+
+// Try to get left (right) chopstick. Returns true on success.
+bool Philosopher::get_left( ) {
+	have_l = left->try_pick_up( );
+	return have_l;
+}
+
+bool Philosopher::get_right( ) {
+	have_r = right->try_pick_up( );
+	return have_r;
+}
+
+// Getters for having chopsticks
+bool Philosopher::have_left( ) {
+	return have_l;
+}
+
+bool Philosopher::have_right( ) {
+	return have_r;
+}
+
+// Eat! Tries to get both chopsticks, sleeps the thread while eating,
+// and then puts down both chopsticks.
+bool Philosopher::eat( ) {
+	if ( (!have_l && !get_left( )) || (!have_r && !get_right( )) ) {
+		++hunger;
+		return false;
+	}
+
+	std::this_thread::sleep_for(eat_time);
+
+	hunger = 0;
+
+	left->set_down( );
+	have_l = false;
+	right->set_down( );
+	have_r = false;
+
+	return true;
+}
+
+// Think! Keep chopsticks in hand while doing so
+void Philosopher::think( ) {
+	std::this_thread::sleep_for(think_time);
+	++hunger;
+}
+
+void Philosopher::sympose(
+		std::default_random_engine& engine,
+		bool &stop,
+		std::atomic<bool> &pause,
+		std::atomic<int> &waiting,
+		std::mutex &eat_exclusion) {
+	int sleepo = std::uniform_int_distribution<int>(0,255)(engine);
+	std::this_thread::sleep_for(std::chrono::milliseconds(sleepo));
+	while (!stop) {
+		if (pause) {
+			waiting++;
+			while(pause)
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+
+
+		int hunger_chance = 1 << hunger;
+		if (std::uniform_int_distribution<int>(0,hunger_chance)(engine) == 0) {
+			think( );
+		} else {
+			eat_exclusion.lock( );
+			bool has_et = eat( );
+			eat_exclusion.unlock( );
+		}
+	}
+}
+
+std::string Philosopher::get_name( ) {
+	return name;
+}
+
+int Philosopher::get_hunger( ) {
+	return hunger;
+}
---
+++ src_mutex/table.cpp
@@ -0,0 +1,87 @@
+/*
+ * table.cpp
+ *
+ *	Definition of the table at which our philosophers are eating
+ *
+ *  Created on: 4 Feb 2020
+ *      Author: tim
+ */
+
+#include "table.hpp"
+#include "visualise.hpp"
+
+#include <random>
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <list>
+#include <iostream>
+
+void randomize_philosophize(
+		Philosopher &phil,
+		int i,
+		bool &stop,
+		std::atomic<bool> &pause,
+		std::atomic<int> &waiting,
+		std::mutex &eat_exclusion);
+void kill_timer(int millis, bool &stop);
+
+Table::Table(std::list<std::string> names)
+: chopsticks( ), philosophers( ), threads(names.size( ))
+{
+	int n_phil = names.size( );
+
+	// Create all chopsticks
+	for (int i = 0; i < n_phil; i++) {
+		chopsticks.emplace_back( );
+	}
+
+	// Create all philosophers, and assign their chopsticks, wrapping the right iterator if the end of the list is reached.
+	int i = 0;
+	auto left = chopsticks.begin();
+	auto right = ++chopsticks.begin();
+	for (auto iname = names.begin(); iname != names.end(); iname++) {
+		philosophers.emplace_back(*iname, &(*left), &( (right != chopsticks.end()) ? *right : chopsticks.front()));
+		++left;
+		++right;
+	}
+	std::cout << "There are " << philosophers.size() << " philosophers." << std::endl;
+}
+
+void Table::sympose( ) {
+	bool stop = false;
+	std::atomic<bool> pause;
+	pause = false;
+	std::atomic<int> waiting;
+	waiting = 0;
+	std::mutex eat_exclusion;
+	// Create the philosopher threads
+	for (int i = 0; i < philosophers.size(); i++) {
+		threads[i] = move(std::thread(randomize_philosophize, std::ref(philosophers[i]), i, std::ref(stop), std::ref(pause), std::ref(waiting), std::ref(eat_exclusion)));
+	}
+	std::thread kill_thread(kill_timer, 3000, std::ref(stop));
+	std::thread visual_thread(visualise, std::ref(philosophers), std::ref(pause), std::ref(waiting), std::ref(stop));
+	// Join the threads
+	for (auto iter = threads.begin(); iter != threads.end(); iter++) {
+		iter->join( );
+	}
+	kill_thread.join( );
+	visual_thread.join();
+
+}
+
+void randomize_philosophize(
+		Philosopher &phil,
+		int i,
+		bool &stop,
+		std::atomic<bool> &pause,
+		std::atomic<int> &waiting,
+		std::mutex &eat_exclusion) {
+	std::default_random_engine engine(time(0)+i);
+	phil.sympose(engine, stop, pause, waiting, eat_exclusion);
+}
+
+void kill_timer(int millis, bool &stop) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(millis));
+	stop = true;
+}
